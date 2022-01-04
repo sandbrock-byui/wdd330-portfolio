959a07c738c370ee26f6b3985bad3f3af858236e
Brandon Rock <sandbrock@byui.edu>
2022-01-03 19:23:53 -0700

    Finished PDF reading exercise

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const renderToc = function () {
+  const links = [
+    {
+      label: "Week 1 Notes",
+      url: "week01/index.html",
+    },
+  ];
+
+  // Get Table of Contents root element
+  const tocRoot = document.getElementById("toc");
+
+  // Add Table of Content links
+  for (const link of links) {
+    const li = document.createElement("li");
+    const { label, url } = link;
+    li.innerHTML = `<a href="${url}">${label}</a>`;
+    tocRoot.appendChild(li);
+  }
+};
+
+renderToc();
